df8ed08e604fc9ec740bac1724a2b82de958aed8
Umeadi, Marlon <umeadi@gmail.com>
2019-07-07 01:51:41 -0500

    updated spacing on console log messages, and fixed all api calls

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log("Download Complete")
+
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+};
+
+exports.omdb = process.env.OMDB_ID;
--- liri.js
+++ liri.js
@@ -1,7 +1,10 @@
 // node liri.js moviethis "boss baby"
+var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
 var request = require("request");
 var axios = require("axios");
 var moment = require("moment");
+var fs = require('fs');
 
 
 var arg1 = process.argv[2];
@@ -10,9 +13,9 @@ var arg2 = process.argv.slice(3).join(" ");
 
 
 
-console.log("arg1: " + arg1);
+console.log("Program: " + arg1);
 
-console.log("arg2: " + arg2);
+console.log("Search Query: " + arg2);
 
 
 
@@ -24,7 +27,7 @@ var startProg = function (arg1, arg2) {
             break;
         case "spotify-this-song" :getSongInfo(arg2);
             break;
-        case "do-what-is-says" :getWhatItSays(arg2);
+        case "do-what-it-says" :getWhatItSays(arg2);
             break;
         default:
             console.log("enter the right action")
@@ -38,6 +41,8 @@ function getMovieInfo(movieName) {
     }
 
     var movieUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
+    var divider = "\n-----------------------------------------------------------------------------------------------------------------------------------------------------------";
+
 
     axios.get(movieUrl)
         .then(function (response) {
@@ -45,6 +50,7 @@ function getMovieInfo(movieName) {
         var jsonData = response.data
 
         var movieData = [
+            divider,
             "Title: " + jsonData.Title,
             "Year: " + jsonData.Year,
             "IMDB Rating: " + jsonData.imdbRating,
@@ -52,7 +58,8 @@ function getMovieInfo(movieName) {
             "Country where movie was produced: " + jsonData.Country,
             "Language: " + jsonData.Language,
             "Plot: " + jsonData.Plot,
-            "Actors: " + jsonData.Actors
+            "Actors: " + jsonData.Actors,
+            divider
         ].join("\n\n");
             // console.log(response);
             console.log(movieData);
@@ -70,11 +77,20 @@ function getMovieInfo(movieName) {
 
 
 
-function getConcertInfo(artist){
+function getConcertInfo(artist, concertTime){
     console.log("get band info");
+
+    // if (concertTime === undefined) {
+    //     console.log("---------------------------------------------")
+    //     console.log("\n---------------------------------------------");
+    //     console.log("Artist doesn't appear to have any tour dates. \nPlease search a different artist.");
+    //     console.log("---------------------------------------------");
+    //     console.log("\n---------------------------------------------")
+    // }
     
     
     var concertUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    var divider = "\n--------------------------------------------------------------------";
 
     axios.get(concertUrl)
     .then(function(response){
@@ -83,10 +99,11 @@ function getConcertInfo(artist){
         var concertTime = jsonData.datetime;
 
         var concertData = [
+            divider,
             "Venue: " + jsonData.venue.name,
             "Location: " + jsonData.venue.city,
-            "Time of Event: " + moment(concertTime).format("MM/DD/YYYY h:mm:ss a")
-            
+            "Time of Event: " + moment(concertTime).format("MM/DD/YYYY h:mm:ss a"),
+            divider
         ].join("\n\n");
 
         console.log(concertData);
@@ -94,6 +111,67 @@ function getConcertInfo(artist){
    
 
 };
+
+function getSongInfo(arg2) {
+    var divider = "\n--------------------------------------------------------------------";
+    var Spotify = require("node-spotify-api");
+    var track = arg2;
+    var spotify = new Spotify({
+      id: "8866202a6eb14d1eb27ea164a7f9f481",
+      secret: "cb3e89d8ff274f6c93153316d08d1fc5"
+    });
+
+spotify
+.search({ type: "track", query: track, limit: 1 })
+.then(function(response) {
+
+    // console.log(response);
+    var response = response.tracks.items[0];
+    var musicData = [
+        divider,
+    "Album Name: " + response.album.name,
+    "Song Name: " + response.name,
+    "Artist Name: " + response.artists[0].name,
+    "Preview Link: " + response.external_urls.spotify,
+    divider
+  ].join("\n\n");
+  console.log(musicData);
+})
+.catch(function(error) {
+  console.log(error);
+  getSongInfo("The Sign Ace of Base")
+});
+};
+
+function getWhatItSays() {
+
+    fs.readFile("random.txt", "utf8", function(error, data) {
+   
+       if (error) {
+           return console.log(error);
+       }
+       console.log(data);
+   
+       var dataArr = data.split(","); 
+   
+       console.log(dataArr);
+   
+       var action = dataArr[0];
+       var query = dataArr[1];
+   
+       switch(action) {
+           case "concert-this": getMyBands(query);
+           break;
+           case "movie-this": getMovieInfo(query);
+           break;
+           case "spotify-this-song": getSongInfo(query);
+           break;
+       }
+   
+   
+    });
+
+}
 
 
 startProg(arg1, arg2);
